d0acadb6a5863593d13c202584148e4355192963
zjwernerAUW <162605789+zjwernerAUW@users.noreply.github.com>
2024-03-07 10:49:04 -0600

    Update AerospacePaymentPlan.py

--- AerospacePaymentPlan.py
+++ AerospacePaymentPlan.py
@@ -21,6 +21,8 @@ if risktype == "Launch":
     if endorsementsCheck == True:
       endorsementpremium = st.number_input("Enter the Premium Amount Associated with the Endorsement:",-1000000,1000000,step = .01,key='endorsementpremium')
       endorsementduedate = st.date_input("Enter the Date Payment for the Endorsement is Due:",key='endorsementduedate')
+    else:
+      pass
     submitlaunch = st.form_submit_button()
   
   if submitlaunch == True:
