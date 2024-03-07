6f9d3482c5a091e53acc93de624b11a7ed654f49
zjwernerAUW <162605789+zjwernerAUW@users.noreply.github.com>
2024-03-07 11:11:56 -0600

    Update AerospacePaymentPlan.py

--- AerospacePaymentPlan.py
+++ AerospacePaymentPlan.py
@@ -21,12 +21,12 @@ if risktype == "Launch":
     endorsementsCheck = st.toggle("Select Checkbox If There Are Endorsements On This Spacecraft",False,key='endorsementcheck')
     submitlaunch = st.form_submit_button()
   if submitlaunch:
-    with st.form(key='endorsement'):
-      if endorsementsCheck:
+    if endorsementsCheck:
+      with st.form(key='endorsement'):
         endorsementpremium = st.number_input("Enter the Premium Amount Associated with the Endorsement:",-1000000.00,1000000.00,step = .01,key='endorsementpremium')
         endorsementduedate = st.date_input("Enter the Date Payment for the Endorsement is Due:",key='endorsementduedate')
-      submitendorsement = st.form_submit_button()
-    if submitendorsement:
+        submitendorsement = st.form_submit_button()
+    if (endorsementsCheck = False || (endorsementsCheck = True && submitendorsement = True)):
       if depositPercent == 0:
         result = f"L1{endorsementMap[endorsementsCheck]} 100% Due {totalDue} Days Before Launch"
       else:
